b013933fd7d1c85b56bbd4205320e7b372bbf2b8
Benjamin Huang <coolconfucius@yahoo.com>
2016-03-22 23:43:46 -0700

    refactor app js

--- app.js
+++ app.js
@@ -32,37 +32,11 @@ app.use('/', require('./routes/index'));
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
-  // next(err);
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
     error: err
   });
 });
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
 
 module.exports = app;
