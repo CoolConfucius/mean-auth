2641404a2982f9c64050f7c04f1c291d5e66931f
Benjamin Huang <coolconfucius@yahoo.com>
2016-03-23 17:50:04 -0700

    added register and login routes to routes index js

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,28 @@
+'use strict';
+
 var express = require('express');
+
+var User = require('../models/user');
+
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Mean Stack Todo List' });
 });
+
+router.post('/register', function(req, res, next){
+  User.register(req.body, function(err, user){    
+    res.send(user);    
+  });
+})
+
+router.post('/login', function(req, res, next){
+  User.authenticate(req.body, function(err, user){
+    if (err) return res.status(401).send(err);
+    var token = user.token();
+    console.log('\n\nresponse1: ', res);
+    res.status(200).send(token);
+  });
+})
 
 module.exports = router;
